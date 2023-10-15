3a752acb32f3d3a27d4038d8547ad6bd5de5d526
Jake Polatty <jakepolatty@gmail.com>
2023-10-15 19:10:59 -0400

    Updated project template to use Spring Initializr

---
+++ src/main/java/com/symbolic/symbolic/SymbolicApplication.java
@@ -0,0 +1,13 @@
+package com.symbolic.symbolic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbolicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SymbolicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/symbolic/symbolic/SymbolicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symbolic.symbolic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymbolicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
