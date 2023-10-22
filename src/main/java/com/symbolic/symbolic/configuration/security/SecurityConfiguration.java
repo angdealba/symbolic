bec7c339fe29df66ea04747568a2b6ba017698d7
Jake Polatty <jakepolatty@gmail.com>
2023-10-21 20:35:32 -0400

    Created Spring Security configuration for managing endpoints

---
+++ src/main/java/com/symbolic/symbolic/configuration/security/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package com.symbolic.symbolic.configuration.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/**").permitAll()
+                        .anyRequest().authenticated()
+                ).httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+}
