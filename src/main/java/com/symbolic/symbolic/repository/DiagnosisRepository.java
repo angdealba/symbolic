7642424c9cc10a351c19eb94cafd79d825411dc1
Angeles de Alba <aid2121@columbia.edu>
2023-10-26 10:30:50 -0400

    Modifying diagnosisRepository for data retrieval

--- src/main/java/com/symbolic/symbolic/repository/DiagnosisRepository.java
+++ src/main/java/com/symbolic/symbolic/repository/DiagnosisRepository.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface DiagnosisRepository extends JpaRepository<Diagnosis, Long> {
     List<Diagnosis> findDiagnosesByPatientId(Long patientId);
     List<Diagnosis> findDiagnosesByPractitionerId(Long practitionerId);
+    List<Diagnosis> findDiagnosesByConditionIgnoreCase(String condition);
 }
