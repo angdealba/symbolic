6188c90a27f07d0a9bbd0a9a5ee9d2ac6124d5c9
Jake Polatty <jakepolatty@gmail.com>
2023-11-24 20:11:45 -0500

    Refactored FacilityController for requestbody and error handling

--- src/main/java/com/symbolic/symbolic/controller/InsurancePolicyController.java
+++ src/main/java/com/symbolic/symbolic/controller/InsurancePolicyController.java
@@ -17,11 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * RequestBody object used to represent InsuracePolicy-related requests
+ * RequestBody object used to represent InsurancePolicy-related requests
  */
 class PolicyRequestBody {
   Long id;
