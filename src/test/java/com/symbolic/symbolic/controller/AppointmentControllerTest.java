1b12e6626384b8152c70ab624fecb7e951f21289
Jake Polatty <jakepolatty@gmail.com>
2023-11-27 00:40:05 -0500

    Added AppointmentController tests for 100% branch and line coverage

--- src/test/java/com/symbolic/symbolic/controller/AppointmentControllerTest.java
+++ src/test/java/com/symbolic/symbolic/controller/AppointmentControllerTest.java
@@ -1,13 +1,7 @@
 package com.symbolic.symbolic.controller;
 
-import com.symbolic.symbolic.entity.Diagnosis;
-import com.symbolic.symbolic.entity.MedicalPractitioner;
-import com.symbolic.symbolic.entity.Patient;
-import com.symbolic.symbolic.entity.Prescription;
-import com.symbolic.symbolic.repository.DiagnosisRepository;
-import com.symbolic.symbolic.repository.MedicalPractitionerRepository;
-import com.symbolic.symbolic.repository.PatientRepository;
-import com.symbolic.symbolic.repository.PrescriptionRepository;
+import com.symbolic.symbolic.entity.*;
+import com.symbolic.symbolic.repository.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -34,21 +28,23 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class DiagnosisControllerTest {
+public class AppointmentControllerTest {
   @Autowired
   private MockMvc mockMvc;
   @MockBean
-  DiagnosisRepository diagnosisRepository;
+  AppointmentRepository appointmentRepository;
   @MockBean
   PatientRepository patientRepository;
   @MockBean
   MedicalPractitionerRepository practitionerRepository;
+  @MockBean
+  FacilityRepository facilityRepository;
   @InjectMocks
-  DiagnosisController diagnosisController;
+  AppointmentController appointmentController;
 
   AutoCloseable openMocks;
 
-  private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+  private SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
 
   @BeforeEach
   public void setUp() {
@@ -65,53 +61,53 @@ public class DiagnosisControllerTest {
     // Test valid ID
     UUID id = UUID.randomUUID();
     String idString = id.toString();
-    assertEquals(id, DiagnosisController.parseUuidFromString(idString));
+    assertEquals(id, AppointmentController.parseUuidFromString(idString));
 
     // Test invalid IDs
-    assertNull(DiagnosisController.parseUuidFromString("test"));
-    assertNull(DiagnosisController.parseUuidFromString("2"));
+    assertNull(AppointmentController.parseUuidFromString("test"));
+    assertNull(AppointmentController.parseUuidFromString("2"));
   }
 
   @Test
-  public void testGetAllDiagnoses() throws Exception {
-    Date date1 = formatter.parse("2023-10-20");
-    Diagnosis diagnosis1 = new Diagnosis("COVID-19", "In-Patient Treatment", date1);
-    Date date2 = formatter.parse("2023-11-21");
-    Diagnosis diagnosis2 = new Diagnosis("Influenza", "Antiviral Medication", date2);
-    List<Diagnosis> diagnoses = new ArrayList<>();
-    when(diagnosisRepository.findAll()).thenReturn(diagnoses);
+  public void testGetAllAppointments() throws Exception {
+    Date date1 = formatter.parse("2023-10-20 12:00");
+    Appointment appointment1 = new Appointment(date1, 100);
+    Date date2 = formatter.parse("2023-11-21 13:30");
+    Appointment appointment2 = new Appointment(date2, 50);
+    List<Appointment> appointments = new ArrayList<>();
+    when(appointmentRepository.findAll()).thenReturn(appointments);
 
-    // Test when no diagnoses exist
-    mockMvc.perform(get("/api/diagnoses")
+    // Test when no appointments exist
+    mockMvc.perform(get("/api/appointments")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNoContent());
 
 
-    // Test when diagnoses are returned
-    diagnoses.add(diagnosis1);
-    diagnoses.add(diagnosis2);
+    // Test when prescriptions are returned
+    appointments.add(appointment1);
+    appointments.add(appointment2);
 
-    mockMvc.perform(get("/api/diagnoses")
+    mockMvc.perform(get("/api/appointments")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isOk());
   }
 
   @Test
-  public void testGetDiagnosisById() throws Exception {
-    Date date1 = formatter.parse("2023-10-20");
-    Diagnosis diagnosis1 = new Diagnosis("COVID-19", "In-Patient Treatment", date1);
+  public void testGetAppointmentById() throws Exception {
+    Date date1 = formatter.parse("2023-10-20 12:00");
+    Appointment appointment1 = new Appointment(date1, 100);
     UUID id = UUID.randomUUID();
-    diagnosis1.setId(id);
-    when(diagnosisRepository.findById(id)).thenReturn(Optional.of(diagnosis1));
+    appointment1.setId(id);
+    when(appointmentRepository.findById(id)).thenReturn(Optional.of(appointment1));
 
-    // Test retrieving a diagnosis with a valid id
-    mockMvc.perform(get("/api/diagnosis")
+    // Test retrieving a appointment with a valid id
+    mockMvc.perform(get("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id + "\"}"))
         .andExpect(status().isOk());
 
     // Test retrieving with no ID
-    MvcResult result1 = mockMvc.perform(get("/api/diagnosis")
+    MvcResult result1 = mockMvc.perform(get("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{}"))
         .andExpect(status().isBadRequest())
@@ -119,7 +115,7 @@ public class DiagnosisControllerTest {
     assertEquals("Missing 'id' field in request body", result1.getResponse().getContentAsString());
 
     // Test retrieving with an invalid ID
-    MvcResult result2 = mockMvc.perform(get("/api/diagnosis")
+    MvcResult result2 = mockMvc.perform(get("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\":  \"test\"}"))
         .andExpect(status().isBadRequest())
@@ -128,85 +124,80 @@ public class DiagnosisControllerTest {
 
     // Test retrieving with a UUID that is not in the database
     UUID id2 = UUID.randomUUID();
-    MvcResult result3 = mockMvc.perform(get("/api/diagnosis")
+    MvcResult result3 = mockMvc.perform(get("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id2 + "\"}"))
         .andExpect(status().isNotFound())
         .andReturn();
-    assertEquals("No diagnosis found with id " + id2, result3.getResponse().getContentAsString());
+    assertEquals("No appointment found with id " + id2, result3.getResponse().getContentAsString());
   }
 
   @Test
-  public void testCreateDiagnosis() throws Exception {
-    // Create valid diagnosis
-    mockMvc.perform(post("/api/diagnosis")
+  public void testCreateAppointment() throws Exception {
+    // Create valid appointment
+    mockMvc.perform(post("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"date\": \"2023-10-20\", " +
-                "\"condition\": \"COVID-19\", " +
-                "\"treatmentInfo\": \"In-Patient Treatment\"}"))
+            .content("{\"dateTime\": \"2023-10-20 12:00\", " +
+                "\"cost\": \"100\"}"))
         .andExpect(status().isCreated());
 
-    // Creating diagnoses with missing fields
-    MvcResult result1 = mockMvc.perform(post("/api/diagnosis")
+    // Creating appointments with missing fields
+    MvcResult result1 = mockMvc.perform(post("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"condition\": \"COVID-19\", " +
-                "\"treatmentInfo\": \"In-Patient Treatment\"}"))
+            .content("{\"cost\": \"100\"}"))
         .andExpect(status().isBadRequest())
         .andReturn();
-    assertEquals("Missing 'date' field in request body", result1.getResponse().getContentAsString());
+    assertEquals("Missing 'dateTime' field in request body", result1.getResponse().getContentAsString());
 
-    MvcResult result2 = mockMvc.perform(post("/api/diagnosis")
+    MvcResult result2 = mockMvc.perform(post("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"date\": \"2023-10-20\", " +
-                "\"treatmentInfo\": \"In-Patient Treatment\"}"))
+            .content("{\"dateTime\": \"2023-10-20 12:00\"}"))
         .andExpect(status().isBadRequest())
         .andReturn();
-    assertEquals("Missing 'condition' field in request body", result2.getResponse().getContentAsString());
+    assertEquals("Missing 'cost' field in request body", result2.getResponse().getContentAsString());
 
-    MvcResult result3 = mockMvc.perform(post("/api/diagnosis")
+    // Test malformed date error cases
+    MvcResult result3 = mockMvc.perform(post("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"date\": \"2023-10-20\", " +
-                "\"condition\": \"COVID-19\"}"))
+            .content("{\"dateTime\": \"2023-10-20\", " +
+                "\"cost\": \"100\"}"))
         .andExpect(status().isBadRequest())
         .andReturn();
-    assertEquals("Missing 'treatmentInfo' field in request body", result3.getResponse().getContentAsString());
+    assertEquals("'dateTime' field value must be in the format yyyy-MM-dd HH:mm", result3.getResponse().getContentAsString());
 
-    // Test malformed date error case
-    MvcResult result4 = mockMvc.perform(post("/api/diagnosis")
+    MvcResult result4 = mockMvc.perform(post("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
-            .content("{\"date\": \"2023-1a-21\", " +
-                "\"condition\": \"COVID-19\", " +
-                "\"treatmentInfo\": \"In-Patient Treatment\"}"))
+            .content("{\"dateTime\": \"2023-10-20 1a:00\", " +
+                "\"cost\": \"100\"}"))
         .andExpect(status().isBadRequest())
         .andReturn();
-    assertEquals("'date' field value must be in the format yyyy-MM-dd", result4.getResponse().getContentAsString());
+    assertEquals("'dateTime' field value must be in the format yyyy-MM-dd HH:mm", result4.getResponse().getContentAsString());
   }
 
   @Test
-  public void testUpdateDiagnosis() throws Exception {
-    Date date1 = formatter.parse("2023-10-20");
-    Diagnosis diagnosis1 = new Diagnosis("COVID-19", "In-Patient Treatment", date1);
+  public void testUpdateAppointment() throws Exception {
+    Date date1 = formatter.parse("2023-10-20 12:00");
+    Appointment appointment1 = new Appointment(date1, 100);
     UUID id = UUID.randomUUID();
-    diagnosis1.setId(id);
-    when(diagnosisRepository.findById(id)).thenReturn(Optional.of(diagnosis1));
+    appointment1.setId(id);
+    when(appointmentRepository.findById(id)).thenReturn(Optional.of(appointment1));
 
-    // Test updating a diagnosis with a valid id
-    mockMvc.perform(put("/api/diagnosis")
+    // Test updating a appointment with a valid id
+    mockMvc.perform(put("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id + "\", " +
-                "\"date\": \"2023-11-21\", " +
-                "\"condition\": \"Influenza\", " +
-                "\"treatmentInfo\": \"Antiviral Medication\"}"))
+                "\"dateTime\": \"2023-11-21 13:30\", " +
+                "\"cost\": \"50\"}"))
         .andExpect(status().isOk());
 
     // Test updating with no fields does not raise error
-    mockMvc.perform(put("/api/diagnosis")
+    mockMvc.perform(put("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id + "\"}"))
         .andExpect(status().isOk());
 
     // Test updating with no ID
-    MvcResult result1 = mockMvc.perform(put("/api/diagnosis")
+    MvcResult result1 = mockMvc.perform(put("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{}"))
         .andExpect(status().isBadRequest())
@@ -214,7 +205,7 @@ public class DiagnosisControllerTest {
     assertEquals("Missing 'id' field in request body", result1.getResponse().getContentAsString());
 
     // Test updating with an invalid ID
-    MvcResult result2 = mockMvc.perform(put("/api/diagnosis")
+    MvcResult result2 = mockMvc.perform(put("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\":  \"test\"}"))
         .andExpect(status().isBadRequest())
@@ -223,41 +214,40 @@ public class DiagnosisControllerTest {
 
     // Test retrieving with a UUID that is not in the database
     UUID id2 = UUID.randomUUID();
-    MvcResult result3 = mockMvc.perform(put("/api/diagnosis")
+    MvcResult result3 = mockMvc.perform(put("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id2 + "\"}"))
         .andExpect(status().isNotFound())
         .andReturn();
-    assertEquals("No diagnosis found with id " + id2, result3.getResponse().getContentAsString());
+    assertEquals("No appointment found with id " + id2, result3.getResponse().getContentAsString());
 
     // Test updating with a malformed date raises an error
-    MvcResult result4 = mockMvc.perform(put("/api/diagnosis")
+    MvcResult result4 = mockMvc.perform(put("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id + "\", " +
-                "\"date\": \"2023-11a-21\", " +
-                "\"condition\": \"Influenza\", " +
-                "\"treatmentInfo\": \"Antiviral Medication\"}"))
+                "\"dateTime\": \"2023-11-21 1a:30\", " +
+                "\"cost\": \"50\"}"))
         .andExpect(status().isBadRequest())
         .andReturn();
-    assertEquals("'date' field value must be in the format yyyy-MM-dd", result4.getResponse().getContentAsString());
+    assertEquals("'dateTime' field value must be in the format yyyy-MM-dd HH:mm", result4.getResponse().getContentAsString());
   }
 
   @Test
-  public void testDeleteDiagnosis() throws Exception {
-    Date date1 = formatter.parse("2023-10-20");
-    Diagnosis diagnosis1 = new Diagnosis("COVID-19", "In-Patient Treatment", date1);
+  public void testDeleteAppointment() throws Exception {
+    Date date1 = formatter.parse("2023-10-20 12:00");
+    Appointment appointment1 = new Appointment(date1, 100);
     UUID id = UUID.randomUUID();
-    diagnosis1.setId(id);
-    when(diagnosisRepository.findById(id)).thenReturn(Optional.of(diagnosis1));
+    appointment1.setId(id);
+    when(appointmentRepository.findById(id)).thenReturn(Optional.of(appointment1));
 
-    // Test deleting a diagnosis with a valid id
-    mockMvc.perform(delete("/api/diagnosis")
+    // Test deleting a appointment with a valid id
+    mockMvc.perform(delete("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id + "\"}"))
         .andExpect(status().isNoContent());
 
     // Test deleting with no ID
-    MvcResult result1 = mockMvc.perform(delete("/api/diagnosis")
+    MvcResult result1 = mockMvc.perform(delete("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{}"))
         .andExpect(status().isBadRequest())
@@ -265,7 +255,7 @@ public class DiagnosisControllerTest {
     assertEquals("Missing 'id' field in request body", result1.getResponse().getContentAsString());
 
     // Test deleting with an invalid ID
-    MvcResult result2 = mockMvc.perform(delete("/api/diagnosis")
+    MvcResult result2 = mockMvc.perform(delete("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\":  \"test\"}"))
         .andExpect(status().isBadRequest())
@@ -274,49 +264,55 @@ public class DiagnosisControllerTest {
 
     // Test deleting a UUID that is not in the database
     UUID id2 = UUID.randomUUID();
-    MvcResult result3 = mockMvc.perform(delete("/api/diagnosis")
+    MvcResult result3 = mockMvc.perform(delete("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id2 + "\"}"))
         .andExpect(status().isNotFound())
         .andReturn();
-    assertEquals("No diagnosis found with id " + id2, result3.getResponse().getContentAsString());
+    assertEquals("No appointment found with id " + id2, result3.getResponse().getContentAsString());
 
-    // Test deleting patient and practitioner fields
+    // Test deleting patient, practitioner, and facility fields
     Patient patient = new Patient("COVID-19", "Dairy", "Wheelchair Access");
     MedicalPractitioner practitioner = new MedicalPractitioner(40.7, 74.0, "Surgery", 50, 10);
-    diagnosis1.setPatient(patient);
-    diagnosis1.setPractitioner(practitioner);
-    mockMvc.perform(delete("/api/diagnosis")
+    Facility facility = new Facility(40.7, 74.0, "Surgery");
+    appointment1.setPatient(patient);
+    appointment1.setPractitioner(practitioner);
+    appointment1.setFacility(facility);
+    mockMvc.perform(delete("/api/appointment")
             .contentType(MediaType.APPLICATION_JSON)
             .content("{\"id\": \"" + id + "\"}"))
         .andExpect(status().isNoContent());
-    assertFalse(patient.getPrescriptions().contains(diagnosis1));
-    assertFalse(practitioner.getPrescriptions().contains(diagnosis1));
+    assertFalse(patient.getAppointments().contains(appointment1));
+    assertFalse(practitioner.getAppointments().contains(appointment1));
+    assertFalse(facility.getAppointments().contains(appointment1));
   }
 
   @Test
-  public void testDeleteAllDiagnoses() throws Exception {
-    Date date1 = formatter.parse("2023-10-20");
-    Diagnosis diagnosis1 = new Diagnosis("COVID-19", "In-Patient Treatment", date1);
-    Date date2 = formatter.parse("2023-11-21");
-    Diagnosis diagnosis2 = new Diagnosis("Influenza", "Antiviral Medication", date2);
-    List<Diagnosis> diagnoses = new ArrayList<>();
-    diagnoses.add(diagnosis1);
-    diagnoses.add(diagnosis2);
-    when(diagnosisRepository.findAll()).thenReturn(diagnoses);
+  public void testDeleteAllAppointments() throws Exception {
+    Date date1 = formatter.parse("2023-10-20 12:00");
+    Appointment appointment1 = new Appointment(date1, 100);
+    Date date2 = formatter.parse("2023-11-21 13:30");
+    Appointment appointment2 = new Appointment(date2, 50);
+    List<Appointment> appointments = new ArrayList<>();
+    when(appointmentRepository.findAll()).thenReturn(appointments);
+    appointments.add(appointment1);
+    appointments.add(appointment2);
+    when(appointmentRepository.findAll()).thenReturn(appointments);
 
     // Test deleting all diagnoses
-    mockMvc.perform(delete("/api/diagnoses")
+    mockMvc.perform(delete("/api/appointments")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNoContent());
 
-    // Test deleting patient and practitioner fields
+    // Test deleting patient, practitioner, and facility fields
     Patient patient = new Patient("COVID-19", "Dairy", "Wheelchair Access");
     MedicalPractitioner practitioner = new MedicalPractitioner(40.7, 74.0, "Surgery", 50, 10);
-    diagnosis1.setPatient(patient);
-    diagnosis1.setPractitioner(practitioner);
+    Facility facility = new Facility(40.7, 74.0, "Surgery");
+    appointment1.setPatient(patient);
+    appointment1.setPractitioner(practitioner);
+    appointment1.setFacility(facility);
 
-    mockMvc.perform(delete("/api/diagnoses")
+    mockMvc.perform(delete("/api/appointments")
             .contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNoContent());
   }
