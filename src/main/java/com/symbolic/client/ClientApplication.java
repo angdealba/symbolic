599d530c6d427c952885c2380589b214fbc4e6e7
Jake Polatty <jakepolatty@gmail.com>
2023-11-29 14:18:57 -0500

    Resolved conflicts between client and service execution

--- src/main/java/com/symbolic/client/ClientApplication.java
+++ src/main/java/com/symbolic/client/ClientApplication.java
@@ -2,8 +2,10 @@ package com.symbolic.client;
 
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application-client.properties")
 public class ClientApplication {
     public static void main(String[] args) {
         Application.launch(BgcheckApplication.class, args);
